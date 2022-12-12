514cb5ab741a121e8c3fbb9de87d5fc973ce5aeb
oumytoure <toureoumy12@gmail.com>
2022-12-12 09:56:37 +0000

    ajout

--- notreprojet/app/Http/Controllers/api/PostController.php
+++ notreprojet/app/Http/Controllers/api/PostController.php
@@ -71,11 +71,7 @@ class PostController extends Controller
 
     public function Search(Request $request)
     {
-        /* $words='%'. $this->$query.'%';
-        if(strlen($this->query)<2){
-           $this->$users=user::where('nom', 'LIKE',$words)
-            ->get();
-        } */
+        
 
         $users = assane::all();
 
@@ -91,14 +87,14 @@ class PostController extends Controller
 
     public function user()
     {
-        //$users = assane::all();
+        
 
         /*return response ()->json($user);*/
         $users = assane::paginate(5);
         //dd($user->links());
        return view('user',['users' => $users]);
 
-        //return view('admin',['user' => $user]);
+        
     }
 
     public function archive()
@@ -110,9 +106,6 @@ class PostController extends Controller
         //dd($user->links());
        return view('archive',['users' => $users]);
 
-        //return view('admin',['user' => $user]);
-        //$users = DB::statement('assane')->orderBy('id','desc')->paginate(5);
-        //return view('admin',compact('users'));
     }
 
     // app > http > controllers > EmployeeController.php
@@ -229,11 +222,6 @@ class PostController extends Controller
     
     public function chercheUser(Request $request)
     {
-        /* $words='%'. $this->$query.'%';
-        if(strlen($this->query)<2){
-           $this->$users=user::where('nom', 'LIKE',$words)
-            ->get();
-        } */
 
         $users = assane::all();
 
